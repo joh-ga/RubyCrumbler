9cef67984df50c73ef4af1f4ba0551f48876dc42
laurabernardy <lauramariabernardy@gmail.com>
2022-01-13 12:49:51 +0100

    Create Input.rb
    Input: an URL or textfile in HTML/XML or Plain Text; Output: Plain Text in txt-File

---
+++ Input.rb
@@ -0,0 +1,20 @@
+require 'readability'
+require 'open-uri'
+require 'nokogiri'
+
+input = 'https://en.wikipedia.org/wiki/Computational_linguistics'
+
+if File.file?(input)
+  first = Nokogiri::HTML(File.open(input))
+  doc = first.search('p').map(&:text)
+else
+  first = Nokogiri::HTML(URI.open(input))
+  doc = first.search('p', 'text').map(&:text)
+end
+
+doc = doc.join('')
+doc = doc.gsub('[', '').gsub(']', '').gsub('\n','').gsub(/\d/, '').gsub('()', '')
+
+print doc
+File.write("doc.txt", doc)
+
