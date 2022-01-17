e7a454bcaba62e7f15c4b67a2b5ff2eceaaac86f
joh-ga <72874215+joh-ga@users.noreply.github.com>
2022-01-17 19:12:22 +0100

    Update tokenization.rb

--- tokenization.rb
+++ tokenization.rb
@@ -1,29 +1,35 @@
-# script task: tokenization
-
 require "ruby-spacy"
 
-# source file
-input = File.open("doc.txt", "r") # probably better to work with file path later
-file = input.read()
-input.close()
+class Tokenization
+  def initialize()
+    @en = Spacy::Language.new("en_core_web_lg")
+  end
 
-# tokenization
-nlp = Spacy::Language.new("en_core_web_lg")
-doc = nlp.read(file)
-row = []
-count = 0
-doc.each do |token|
-  count += 1
-  row << token.text
+  def tokenizer(filename)
+    input = File.open(filename, "r")
+    file = input.read()
+    input.close()
+
+    # tokenization
+    doc = @en.read(file)
+    row = []
+    count = 0
+    doc.each do |token|
+      count += 1
+      row << token.text
+    end
+
+    # write tokenized content into new output file
+    name = filename.sub(/(?<=.)\..*/, '')
+    File.open("#{name}_tokenization.txt", "w") do |f|
+      f.write(row)
+    f.write("\n")
+    f.write("\n")
+    f.write("Total number of tokens: #{count}")
+    end
+  end
 end
 
-# write tokenized content into new output file
-File.open("tokenization.txt", "w") do |f|
-  f.write(row)
-  f.write("\n")
-  f.write("\n")
-  f.write("Total number of tokens: #{count}")
-end
-
-
-
+fileobject = "doc.txt"
+neu = Tokenization.new
+neu.tokenizer(fileobject)
