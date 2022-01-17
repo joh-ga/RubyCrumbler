e5a794f7412bfd80e662bb1fbc8a5cf20c1d6904
laurabernardy <lauramariabernardy@gmail.com>
2022-01-17 10:06:25 +0100

    Update Input.rb
    datainput as method

--- Input.rb
+++ Input.rb
@@ -1,18 +1,22 @@
+require 'readability'
 require 'open-uri'
 require 'nokogiri'
 
-input = 'https://en.wikipedia.org/wiki/Computational_linguistics'
+def datainput (input, outputname)
 
-if File.file?(input)
-  first = Nokogiri::HTML(File.open(input))
-  doc = first.search('p').map(&:text)
-else
-  first = Nokogiri::HTML(URI.open(input))
-  doc = first.search('p', 'text').map(&:text)
+  if File.file?(input)
+    first = Nokogiri::HTML(File.open(input))
+    doc = first.search('p').map(&:text)
+  else
+    first = Nokogiri::HTML(URI.open(input))
+    doc = first.search('p', 'text').map(&:text)
+  end
+
+  doc = doc.join('').gsub(/\d/, '').gsub('[','').gsub(']','').gsub('(','').gsub(')','')
+  print doc
+  File.write("#{outputname}.txt", doc)
 end
 
-doc = doc.join('').gsub(/\d/, '').gsub('[','').gsub(']','').gsub('(','').gsub(')','')
 
-print doc
-File.write("doc.txt", doc)
-
+#neu = datainput('https://en.wikipedia.org/wiki/Computational_linguistics', 'output')
+#p neu
