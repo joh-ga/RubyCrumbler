2484a65414cf3658623cfed051e26d85c50c4f4a
laurabernardy <lauramariabernardy@gmail.com>
2022-01-17 11:14:08 +0100

    Update Input.rb
    Input and text cleaning methods.  sentence ending dots and three kinds of apostrophes are ignored while cleaning (-> needed for tokenization tasks)

--- Input.rb
+++ Input.rb
@@ -2,21 +2,34 @@ require 'readability'
 require 'open-uri'
 require 'nokogiri'
 
-def datainput (input, outputname)
-
-  if File.file?(input)
-    first = Nokogiri::HTML(File.open(input))
-    doc = first.search('p').map(&:text)
-  else
-    first = Nokogiri::HTML(URI.open(input))
-    doc = first.search('p', 'text').map(&:text)
-  end
-
-  doc = doc.join('').gsub(/\d/, '').gsub('[','').gsub(']','').gsub('(','').gsub(')','')
-  print doc
-  File.write("#{outputname}.txt", doc)
+class Doctoclean
+def initialize(input, outputname)
+  @output = outputname
+  @input = input
+  @text2clean
 end
 
+def datainput()
+  if File.file?(@input)
+    first = Nokogiri::HTML(File.open(@input))
+    doc = first.search('p').map(&:text)
+  else
+    first = Nokogiri::HTML(URI.open(@input))
+    doc = first.search('p', 'text').map(&:text)
+  end
+  #print doc
+  File.write("#{@output}.txt", doc)
+end
 
-#neu = datainput('https://en.wikipedia.org/wiki/Computational_linguistics', 'output')
-#p neu
+def cleantext()
+  @text2clean = File.open("#{@output}.txt", 'r')
+  @text2clean = File.read(@text2clean)
+  @text2clean = @text2clean.gsub('\n','').gsub(/https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,}/,'').gsub(/\d/, '').gsub(/[^\w\s\.\'´`]/,'').gsub(/[ ]{2,}/,' ').gsub(/[\.]{2,}/,' ')
+  p @text2clean
+  File.write("#{@output}_clean.txt", @text2clean)
+end
+end
+
+neu = Doctoclean.new('https://blog.francium.tech/ruby-getting-clean-content-from-html-page-a5af2e3baaf0', "outputdoc")
+neu.datainput()
+neu.cleantext()
