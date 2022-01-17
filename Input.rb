66033fb1b8d06b71fe1304f2f9ef5eb678bc1013
laurabernardy <lauramariabernardy@gmail.com>
2022-01-17 11:48:28 +0100

    Update Input.rb
    Input and text cleaning methods.  sentence ending dots and three kinds of apostrophes are ignored while cleaning (-> needed for tokenization tasks); cleans also wrong character encoding

--- Input.rb
+++ Input.rb
@@ -23,12 +23,12 @@ end
 def cleantext()
   @text2clean = File.open("#{@output}.txt", 'r')
   @text2clean = File.read(@text2clean)
-  @text2clean = @text2clean.gsub('\n','').gsub(/https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,}/,'').gsub(/\d/, '').gsub(/[^\w\s\.\'´`]/,'').gsub(/[ ]{2,}/,' ').gsub(/[\.]{2,}/,' ')
+  @text2clean = @text2clean.gsub('\n','').gsub('\r','').gsub(/\\u[a-f0-9]{4}/i,'').gsub(/https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,}/,'').gsub(/\d/, '').gsub(/[^\w\s\.\'´`]/,'').gsub(/[\.]{2,}/,' ').gsub(/[ ]{2,}/,' ')
   p @text2clean
   File.write("#{@output}_clean.txt", @text2clean)
 end
 end
 
-neu = Doctoclean.new('https://blog.francium.tech/ruby-getting-clean-content-from-html-page-a5af2e3baaf0', "outputdoc")
+neu = Doctoclean.new('https://stackoverflow.com/questions/18492664/ruby-output-unicode-character', "outputdoc")
 neu.datainput()
 neu.cleantext()
