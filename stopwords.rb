3e72a18222c78f10fd5f86646c59616e8c919dc0
laurabernardy <lauramariabernardy@gmail.com>
2022-01-17 18:09:02 +0100

    Update stopwords.rb
    remove stopwords from spacy stopwordslist, add new stopwords and print cleaned text as array into txt-file

--- stopwords.rb
+++ stopwords.rb
@@ -4,17 +4,16 @@ class NoStops
     en = Spacy::Language.new("en_core_web_lg")
     stopwords = en.Defaults.stop_words.to_s.gsub('\'','"').delete('{}" ').gsub('’','\'')
     @stopwords = stopwords.split(',')
-    print @stopwords
   end
 
   def stopwordsclean()
     text = File.open("tokenization.txt", "r")
-    text = File.read(text).downcase.delete('[]" ')
-    text = text.split(',')
+    text = File.read(text).gsub(/Total number of tokens: \d+/, '')
+    text = Kernel.eval(text)
 
     shared = text & @stopwords
     textosw = text - shared
-    File.write("NoStopwords.txt", textosw.join(', '))
+    File.write("NoStopwords.txt", textosw)
   end
 
   def add_stopwords(newsw)
@@ -25,5 +24,4 @@ end
 
 
 neu = NoStops.new
-neu.add_stopwords("")
 neu.stopwordsclean
