55831515d0485d72401def102117d2ac857b6d34
joh-ga <72874215+joh-ga@users.noreply.github.com>
2022-01-17 15:50:24 +0100

    Add files via upload

---
+++ tokenization.rb
@@ -0,0 +1,30 @@
+# script task: tokenization
+
+require "ruby-spacy"
+require "terminal-table"
+
+# source file
+input = File.open("doc.txt", "r") # probably better to work with file path later
+file = input.read()
+input.close()
+
+# tokenization
+nlp = Spacy::Language.new("en_core_web_lg")
+doc = nlp.read(file)
+row = []
+count = 0
+doc.each do |token|
+  count += 1
+  row << token.text
+end
+
+# write tokenized content into new output file
+File.open("tokenization.txt", "w") do |f|
+  f.write(row)
+  f.write("\n")
+  f.write("\n")
+  f.write("Total number of tokens: #{count}")
+end
+
+
+
